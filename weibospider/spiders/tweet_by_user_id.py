ef22f40ac5ee2ca87255753702df079558c1ca0f
Eli Yip <yezi.2022@outlook.com>
2025-02-02 21:28:54 +0800

    feat: support config via toml for user tweet

--- weibospider/spiders/tweet_by_user_id.py
+++ weibospider/spiders/tweet_by_user_id.py
@@ -5,9 +5,11 @@ Author: nghuyong
 Mail: nghuyong@163.com
 Created Time: 2020/4/14
 """
+
 import datetime
 import json
-import re
+import os
+import toml
 
 from scrapy import Spider
 from scrapy.http import Request
@@ -18,30 +20,58 @@ class TweetSpiderByUserID(Spider):
     """
     用户推文数据采集
     """
+
     name = "tweet_spider_by_user_id"
 
     def start_requests(self):
         """
         爬虫入口
         """
-        # 这里user_ids可替换成实际待采集的数据
-        user_ids = ['1087770692']
-        # 这里的时间替换成实际需要的时间段，如果要采集用户全部推文 is_crawl_specific_time_span 设置为False
-        is_crawl_specific_time_span = True
-        start_time = datetime.datetime(year=2022, month=1, day=1)
-        end_time = datetime.datetime(year=2023, month=1, day=1)
+        # 读取配置文件
+        config_path = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), "config", "spider_config.toml"
+        )
+        config = toml.load(config_path)
+        spider_config = config["tweet_by_user_id"]
+
+        # 从配置中获取参数
+        user_ids = spider_config["user_ids"]
+        is_crawl_specific_time_span = spider_config["is_crawl_specific_time_span"]
+
+        if is_crawl_specific_time_span:
+            time_config = spider_config["time_span"]
+            start_time = datetime.datetime(
+                year=time_config["start_year"],
+                month=time_config["start_month"],
+                day=time_config["start_day"],
+            )
+            end_time = datetime.datetime(
+                year=time_config["end_year"],
+                month=time_config["end_month"],
+                day=time_config["end_day"],
+            )
+
         for user_id in user_ids:
             url = f"https://weibo.com/ajax/statuses/searchProfile?uid={user_id}&page=1&hasori=1&hastext=1&haspic=1&hasvideo=1&hasmusic=1&hasret=1"
             if not is_crawl_specific_time_span:
-                yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': 1})
+                yield Request(
+                    url, callback=self.parse, meta={"user_id": user_id, "page_num": 1}
+                )
             else:
                 # 切分成10天进行
                 tmp_start_time = start_time
                 while tmp_start_time <= end_time:
                     tmp_end_time = tmp_start_time + datetime.timedelta(days=10)
                     tmp_end_time = min(tmp_end_time, end_time)
-                    tmp_url = url + f'&starttime={int(tmp_start_time.timestamp())}&endtime={int(tmp_end_time.timestamp())}'
-                    yield Request(tmp_url, callback=self.parse, meta={'user_id': user_id, 'page_num': 1})
+                    tmp_url = (
+                        url
+                        + f"&starttime={int(tmp_start_time.timestamp())}&endtime={int(tmp_end_time.timestamp())}"
+                    )
+                    yield Request(
+                        tmp_url,
+                        callback=self.parse,
+                        meta={"user_id": user_id, "page_num": 1},
+                    )
                     tmp_start_time = tmp_end_time + datetime.timedelta(days=1)
 
     def parse(self, response, **kwargs):
@@ -49,16 +79,20 @@ class TweetSpiderByUserID(Spider):
         网页解析
         """
         data = json.loads(response.text)
-        tweets = data['data']['list']
+        tweets = data["data"]["list"]
         for tweet in tweets:
             item = parse_tweet_info(tweet)
-            del item['user']
-            if item['isLongText']:
-                url = "https://weibo.com/ajax/statuses/longtext?id=" + item['mblogid']
-                yield Request(url, callback=parse_long_tweet, meta={'item': item})
+            del item["user"]
+            if item["isLongText"]:
+                url = "https://weibo.com/ajax/statuses/longtext?id=" + item["mblogid"]
+                yield Request(url, callback=parse_long_tweet, meta={"item": item})
             else:
                 yield item
         if tweets:
-            user_id, page_num = response.meta['user_id'], response.meta['page_num']
-            url = response.url.replace(f'page={page_num}', f'page={page_num + 1}')
-            yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': page_num + 1})
+            user_id, page_num = response.meta["user_id"], response.meta["page_num"]
+            url = response.url.replace(f"page={page_num}", f"page={page_num + 1}")
+            yield Request(
+                url,
+                callback=self.parse,
+                meta={"user_id": user_id, "page_num": page_num + 1},
+            )
